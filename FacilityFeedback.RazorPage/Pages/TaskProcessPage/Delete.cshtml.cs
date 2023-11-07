bfd44986fb0d84419f1a35c3c3de1cb6827fde51
Hùng Khanh <hungkhanh.lof@gmail.com>
2023-11-07 15:56:02 +0700

    Fix task process

--- FacilityFeedback.RazorPage/Pages/TaskProcessPage/Delete.cshtml.cs
+++ FacilityFeedback.RazorPage/Pages/TaskProcessPage/Delete.cshtml.cs
@@ -13,10 +13,12 @@ namespace FacilityFeedback.RazorPage.Pages.TaskProcessPage
     public class DeleteModel : PageModel
     {
         private readonly ITaskProcessService _service;
+        private readonly IProblemService _problemService;
 
-        public DeleteModel(ITaskProcessService service)
+        public DeleteModel(ITaskProcessService service, IProblemService problemService)
         {
             _service = service;
+            _problemService = problemService;
         }
 
         [BindProperty]
@@ -24,11 +26,12 @@ namespace FacilityFeedback.RazorPage.Pages.TaskProcessPage
 
         public async Task<IActionResult> OnGetAsync(int id)
         {
+            var problem = await _problemService.GetAllNoPaging();   
             var taskProcess = await _service.GetById(id);
-
+            
             if (taskProcess == null)
                 return NotFound();
-
+            taskProcess.Problem = problem.Where(p => p.Id == taskProcess.ProblemId).FirstOrDefault();
             TaskProcess = taskProcess;
             return Page();
         }
--- FacilityFeedback.RazorPage/Pages/TaskProcessPage/Details.cshtml.cs
+++ FacilityFeedback.RazorPage/Pages/TaskProcessPage/Details.cshtml.cs
@@ -13,19 +13,25 @@ namespace FacilityFeedback.RazorPage.Pages.TaskProcessPage
     public class DetailsModel : PageModel
     {
         private readonly ITaskProcessService _service;
+        private readonly IProblemService _problemService;
 
-        public DetailsModel(ITaskProcessService service)
+
+        public DetailsModel(ITaskProcessService service, IProblemService problemService)
         {
             _service = service;
+            _problemService = problemService;
         }
 
         public TaskProcess TaskProcess { get; set; } = default!;
 
         public async Task<IActionResult> OnGetAsync(int id)
         {
+            var problem = await _problemService.GetAllNoPaging();
             var taskProcess = await _service.GetById(id);
             if (taskProcess == null)
                 return NotFound();
+            taskProcess.Problem = problem.Where(p => p.Id == taskProcess.ProblemId).FirstOrDefault();
+
             TaskProcess = taskProcess;
             return Page();
         }
